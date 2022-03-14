57d52cacf2a0c9684ab15b600d96f85d0ef557c5
Andrei Tanana <tanana@email.ru>
2022-03-14 16:18:48 +0300

    currency selector init

--- app/src/main/java/com/atanana/currencycompose/MainActivity.kt
+++ app/src/main/java/com/atanana/currencycompose/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.atanana.currencycompose.ui.CurrencySelector
+import com.atanana.currencycompose.ui.CurrencySelectorState
 import com.atanana.currencycompose.ui.theme.CurrencyComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -26,22 +25,9 @@ class MainActivity : ComponentActivity() {
             CurrencyComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    CurrencySelector(CurrencySelectorState(0.0, "USD"))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CurrencyComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/atanana/currencycompose/ui/CurrencySelector.kt
@@ -0,0 +1,40 @@
+package com.atanana.currencycompose.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.atanana.currencycompose.ui.theme.CurrencyComposeTheme
+
+@Composable
+fun CurrencySelector(state: CurrencySelectorState) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(16.dp)) {
+        OutlinedTextField(
+            value = state.amount.toString(),
+            onValueChange = {},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        Text(text = state.code)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CurrencySelectorPreview() {
+    CurrencyComposeTheme {
+        CurrencySelector(CurrencySelectorState(0.0, "USD"))
+    }
+}
+
+data class CurrencySelectorState(val amount: Double, val code: String)
