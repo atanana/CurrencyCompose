b1f0a5003e77439ae686f1a6b0b5b4d10a8c31dc
Andrei Tanana <tanana@email.ru>
2022-03-15 13:21:51 +0300

    fix api

--- data/src/main/java/com/atanana/currencycompose/data/network/Api.kt
+++ data/src/main/java/com/atanana/currencycompose/data/network/Api.kt
@@ -1,6 +1,10 @@
 package com.atanana.currencycompose.data.network
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface Api {
 
-    suspend fun getConversions(): ConversionsResult
+    @GET("latest/{currency}")
+    suspend fun getConversions(@Path("currency") currency: String): ConversionsResult
 }
